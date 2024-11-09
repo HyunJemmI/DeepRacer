28ca0142d4275dbc0baaf9dcd6c6b1c8bfb5751c
shhy01 <1208ji@naver.com>
2024-11-09 21:56:50 +0900

    FEAT Integrate PWM.cpp with actuator

--- project/Actuator/src/actuator/aa/utility.cpp
+++ project/Actuator/src/actuator/aa/utility.cpp
@@ -1,10 +1,12 @@
-#include "utility.hpp"
+#include "actuator/aa/utility.hpp"
 
-namespace PWM {
+namespace PWM
+{
     /// Check's if the desired file exist
     /// @returns True if file exist, false otherwise
     /// @param filePath Full path to desired file for which to validate existence
-    bool checkFile(const std::string &filePath) {
+    bool checkFile(const std::string &filePath)
+    {
         int fd = open(filePath.c_str(), O_RDONLY);
         close(fd);
         return fd >= 0;
@@ -13,7 +15,8 @@ namespace PWM {
     /// Writes the given calibration map to file
     /// @param jsonValue json value to write to file
     /// @param filePath Full path to the desired file for which to write the given json.
-    void writeJSONToFile(Json::Value jsonValue, const std::string &filePath) {
+    void writeJSONToFile(Json::Value jsonValue, const std::string &filePath)
+    {
         // Write to file
         Json::StreamWriterBuilder builder;
         builder["commentStyle"] = "None";

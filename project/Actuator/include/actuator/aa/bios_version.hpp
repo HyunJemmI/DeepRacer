e89a387e0f7c0b6d5d61d1ead4929db4b69a5578
shhy01 <1208ji@naver.com>
2024-11-10 21:35:53 +0900

    UPDATE bios_version, json, utility, servo_mgr with actuator

--- project/Actuator/include/actuator/aa/bios_version.hpp
+++ project/Actuator/include/actuator/aa/bios_version.hpp
@@ -4,14 +4,36 @@
 #include <string>
 
 namespace PWM {
- /// This is the hardware rev where we need to start checking
- /// calibration file because of a manufacturing polarity issue
- const std::string BIOS_CUT_OFF = "0.0.8";
- class BiosVersion {
+    /// This is the hardware rev where we need to start checking
+    /// calibration file because of a manufacturing polarity issue
+    const std::string BIOS_CUT_OFF = "0.0.8";
+    class BiosVersion
+    {
     /// Helper class for comparing bios versions
     public:
-        BiosVersion(const std::string &biosVersion);
-        bool operator >= (const BiosVersion &rhs);
+        BiosVersion(const std::string &biosVersion) {
+            int ret = std::sscanf(biosVersion.c_str(), "%d.%d.%d", &first_, &second_, &third_);
+            if (ret <=0) {
+                std::sscanf(BIOS_CUT_OFF.c_str(), "%d.%d.%d", &first_, &second_, &third_);
+            }
+        }
+        bool operator >= (const BiosVersion &rhs) const {
+            if (first_ > rhs.first_) {
+                return true;
+            }
+            if (second_ > rhs.second_) {
+                return true;
+            }
+            if (third_ > rhs.third_) {
+                return true;
+            }
+            if (first_ == rhs.first_
+                && second_ == rhs.second_ &&
+                third_ == rhs.third_) {
+                return true;
+            }
+            return false;
+        }
     private:
         /// Bios version is comprised of three digits first.second.thirds
         /// version comparaon starts with the first digit
